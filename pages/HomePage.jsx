ce68c869d94322f8c4600629c24da966b99621f1
LucaStellato <lucastellato435@gmail.com>
2025-12-11 14:17:38 +0100

    fetch data function and card

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,8 +1,32 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
 
+import axios from 'axios'
 export default function HomePage() {
+    const [movies, setMovies] = useState([]);
+    const endpoint = 'http://localhost:3000/movies'
+    useEffect((fetchMovies) => {
+        axios.get(endpoint)
+            .then(response => {
+                setMovies(response.data);
+            })
+            .catch(error => {
+                console.error("Errore nel recupero dei film:", error);
+            });
+    })
     return (
         <>
+            {movies.map((movie) => (
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">{movie.title}</h5>
+                        <p class="card-text">{movie.abstract}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
 
+            ))}
         </>
     )
-}
+} 
