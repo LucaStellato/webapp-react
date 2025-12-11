68e57ac30e8a663fc788c5d27dc80fe341a99d42
LucaStellato <lucastellato435@gmail.com>
2025-12-11 12:41:54 +0100

    create component to header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
-import DefaultLayout from './layout/DefaultLayout'
+import DefaultLayout from '../pages/DefaultLayout'
 import HomePage from '../pages/HomePage'
 
 function App() {
