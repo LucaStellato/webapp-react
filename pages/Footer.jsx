608d1e7e18ac5af093163ce385dc6ca2f048dcb1
LucaStellato <lucastellato435@gmail.com>
2025-12-12 16:07:56 +0100

    optimization footer

--- pages/Footer.jsx
+++ pages/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <>        <footer className='bg-secondary text-white' style={{ minHeight: '100px' }}>
             <div className='row'>
-                <h4 className='mt-4 bg-secondary text-white'>se hai bisogno contattaci su:</h4>
+                <h4 className='mt-4 bg-secondary text-white ms-3'>se hai bisogno contattaci su:</h4>
                 <div className="d-flex justify-content-center" style={{
                     color: 'white'
                 }}>
