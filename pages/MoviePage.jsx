252d593f0dfab84cd40b829f52bca9a355392b85
LucaStellato <lucastellato435@gmail.com>
2025-12-12 13:53:17 +0100

    spacing form

--- pages/MoviePage.jsx
+++ pages/MoviePage.jsx
@@ -19,13 +19,40 @@ export default function MoviePage() {
 
     return (
         <>
-            <div className="container d-flex">
+            <div className="container d-flex mt-2">
 
                 <img src={`${urlImage}${movie.image}`} className="" style={{ width: '200px' }} alt={movie.title} />
+                <div className="justify-content-center ms-5">
+                    <h1>{movie.title}</h1>
+                    <span>{movie.abstract}</span>
 
-
+                </div>
 
             </div>
+            <div className="text-center">
+                <strong>LASCIA UN TUO FEED</strong>
+                <div class="mb-3 m-4">
+                    <label for="name" class="form-label">Insert your name</label>
+                    <input type="text" class="form-control" id="name" placeholder="name" />
+                </div>
+                <div class="mb-3 m-4">
+                    <label for="Select" class="form-label"> select Star</label>
+                    <select id="start" class="form-select">
+                        <option>select</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
+
+                <div class="mb-3 m-4">
+                    <label for="Review" class="form-label">Leave your review</label>
+                    <textarea class="form-control" id="review" rows="3"></textarea>
+                </div>
+            </div>
+
         </>
     )
 }
