2cb2c8c3ef3066a0af1e440dfa7f06c2bae1007b
LucaStellato <lucastellato435@gmail.com>
2025-12-12 15:41:11 +0100

    bug resolved

--- pages/MoviePage.jsx
+++ pages/MoviePage.jsx
@@ -29,53 +29,62 @@ export default function MoviePage() {
     }
 
     return (
+
         <>
-            <div className="container d-flex mt-2">
+            {movie &&
+                <>
+                    <div className="container d-flex mt-2">
 
-                <img src={`${urlImage}${movie.image}`} className="" style={{ width: '200px' }} alt={movie.title} />
-                <div className="justify-content-center ms-5">
-                    <h1>{movie.title}</h1>
-                    <span>{movie.abstract}</span>
 
-                </div>
 
-            </div>
-            <div className="text-center">
-                <strong>LASCIA UN TUO FEED</strong>
-                <div class="mb-3 m-4">
-                    <label for="name" class="form-label">Insert your name</label>
-                    <input type="text" class="form-control" id="name" placeholder="name" />
-                </div>
-                <div class="mb-3 m-4">
-                    <label for="Select" class="form-label"> select Star</label>
-                    <select id="start" class="form-select">
-                        <option>select</option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                </div>
+                        <img src={`${urlImage}${movie.image}`} className="" style={{ width: '200px' }} alt={movie.title} />
 
-                <div class="mb-3 m-4">
-                    <label for="Review" class="form-label">Leave your review</label>
-                    <textarea class="form-control" id="review" rows="3"></textarea>
-                </div>
-            </div>
+                        <div className="justify-content-center ms-5">
+                            <h1>{movie.title}</h1>
+                            <span>{movie.abstract}</span>
 
-            {movie.reviews.map((review) => (
-                <div className="card p-3 mb-3">
-                    <strong>{review.name}</strong>
-                    <p>
-                        {review.text}
-                    </p>
-                    <div className="rating">
-                        {getRating(review.rating)}
+                        </div>
+
+
+
                     </div>
-                </div>
-            ))}
+                    <div className="text-center">
+                        <strong>LASCIA UN TUO FEED</strong>
+                        <div class="mb-3 m-4">
+                            <label for="name" class="form-label">Insert your name</label>
+                            <input type="text" class="form-control" id="name" placeholder="name" />
+                        </div>
+                        <div class="mb-3 m-4">
+                            <label for="Select" class="form-label"> select Star</label>
+                            <select id="start" class="form-select">
+                                <option>select</option>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div>
 
+                        <div class="mb-3 m-4">
+                            <label for="Review" class="form-label">Leave your review</label>
+                            <textarea class="form-control" id="review" rows="3"></textarea>
+                        </div>
+                    </div>
+
+                    {movie && movie.reviews && movie.reviews.map((review) => (
+                        <div className="card p-3 mb-3">
+                            <strong>{review.name}</strong>
+                            <p>
+                                {review.text}
+                            </p>
+                            <div className="rating">
+                                {getRating(review.rating)}
+                            </div>
+                        </div>
+                    ))}
+                </>
+            }
         </>
     )
 }
